41c1fec9c1edc068f080f3d3750d20b7bd32d134
bigtyno <bigtyno4@gmail.com>
2022-06-12 23:31:15 +0900

    ...?

--- src/main/java/com/example/spring/controller/Homecontroller.java
+++ src/main/java/com/example/spring/controller/Homecontroller.java
@@ -2,7 +2,9 @@ package com.example.spring.controller;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping
 @Controller
 public class Homecontroller {
 
